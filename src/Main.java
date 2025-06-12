05938afae045c366acc083b4dffb3674a31791b7
MaryPV <fornewstudies@gmail.com>
2025-06-12 23:51:23 +0500

    HW1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,67 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Классная работа");
         System.out.println("Задача 1");
-
+        /*for (int i = 0; i < 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }*/
+        /*int salary = 65535;
+        int total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
+        System.out.println(total);*/
+        System.out.println("Домашняя работа 1");
+        System.out.println("Задача 1");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 4");
+        for (int i = 10; i > -10; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println("Високосный год " + i);
+        }
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача 8");
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
+        System.out.println("Задача 9");
+        salary = 29000;
+        total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
+        System.out.println("Задача 10");
+        int number = 0;
+        for (int i = 2; i <= 20; i = i + 2) {
+            number = number + 1;
+            System.out.println( number + " * 2 = " + i);
+        }
     }
+
 }
